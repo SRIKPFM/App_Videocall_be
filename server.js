ba38e9a2ee84dbb97f3e9352fdb8e8b14c3fcc9b
Sundaresh-ishop <sundareshishop@gmail.com>
2025-04-04 17:37:41 +0530

    Some changes made successfully

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
+import { initiateApp } from './src/Helper/helper.js';
 dotenv.config();
 
 const app = express();
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(bodyParser.json());
@@ -16,6 +18,8 @@ mongoose.connect(process.env.MONGODBURI)
 .then(() => console.log("Database connected successfully.."))
 .catch((error) => console.log("Database not connected successfully.."));
 
-app.listen(() => {
-    console.log(`Server successfully running on port ${process.env.PORT}`);
+initiateApp(app);
+
+app.listen(port, () => {
+    console.log(`Server successfully running on port ${port}`);
 });

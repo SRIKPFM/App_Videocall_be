8aee049474fad590f9a82af29c97adabbbdd74e7
Sundaresh-ishop <sundareshishop@gmail.com>
2025-04-10 14:59:51 +0530

    Schema and routes for contact

--- src/Helper/helper.js
+++ src/Helper/helper.js
@@ -1,9 +1,12 @@
 import callLogRoutes from '../routes/callLogRouters.js';
 import loginRoutes from '../routes/loginRouters.js';
+import contactRoutes from '../routes/contactRouters.js';
 
 export const initiateApp = (expressApp) => {
 
     expressApp.use(loginRoutes);
 
-    expressApp.use(callLogRoutes)
+    expressApp.use(callLogRoutes);
+
+    expressApp.use(contactRoutes);
 };

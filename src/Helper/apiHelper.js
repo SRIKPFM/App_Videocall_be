3d7adfc86cc417ad99430981165d5d832461ea04
Sundaresh-ishop <sundareshishop@gmail.com>
2025-04-24 13:54:02 +0530

    file craeated for handling API's

---
+++ src/Helper/apiHelper.js
@@ -0,0 +1,24 @@
+import callLogRoutes from '../routes/callLogRouters.js';
+import userRoutes from '../routes/userRouters.js';
+import contactRoutes from '../routes/contactRouters.js';
+import agoraRoutes from '../routes/agoraRouters.js';
+import fcmRoutes from '../routes/fcmRouters.js';
+import messageRoutes from '../routes/MessageRouters.js';
+import cloudinaryRoutes from '../routes/cloudinary.js';
+
+export const initiateApp = (expressApp) => {
+
+    expressApp.use(userRoutes);
+
+    expressApp.use(callLogRoutes);
+
+    expressApp.use(contactRoutes);
+
+    expressApp.use(agoraRoutes);
+
+    expressApp.use(fcmRoutes);
+
+    expressApp.use(messageRoutes);
+
+    expressApp.use(cloudinaryRoutes);
+};

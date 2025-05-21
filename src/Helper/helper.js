ac009d9e1ab8b933c5f9c0f644e5b4616f2fd1e5
Sundaresh-ishop <sundareshishop@gmail.com>
2025-05-21 14:54:12 +0530

    Some functions created for shortcuts

--- src/Helper/helper.js
+++ src/Helper/helper.js
@@ -1,5 +1,6 @@
 import { groupSchema } from "../models/groupMessageModels.js";
-import jwt from 'jsonwebtoken';
+import jwt, { decode } from 'jsonwebtoken';
+import { UserLoginCredentials } from "../models/loginModels.js";
 
 export const getFolderByMimeType = (mimetype) => {
     if (mimetype.startsWith('image/')) return 'images';
@@ -12,13 +13,26 @@ export const generateToken = (data) => {
     const secretKey = process.env.SECRET_KEY_FOR_TOKEN;
     const token = jwt.sign(data, secretKey);
     return token;
-}
+};
 
 export const getUserIdFromToken = (token) => {
     const secretKey = process.env.SECRET_KEY_FOR_TOKEN;
     const getUserId = jwt.verify(token, secretKey);
     return getUserId.userId;
-}
+};
+
+export const getUserDataFromToken = async (token) => {
+    try {
+        const decoded = jwt.decode(token, process.env.SECRET_KEY_FOR_TOKEN);
+        const getUserDetails = await UserLoginCredentials.findOne({ userId: decoded.userId },{ __v: 0, createdAt: 0, updatedAt: 0 });
+        if (!getUserDetails) {
+            return ({ success: false, error: "Can't get user details." });
+        }
+        return getUserDetails;
+    } catch (error) {
+        return ({ success: false, error: error.message });
+    }
+};
 
 export const isGroupExcist = async (groupId) => {
     const isGroupExcist = await groupSchema.findOne({ groupId: groupId });
@@ -26,11 +40,11 @@ export const isGroupExcist = async (groupId) => {
         return res.status(404).json({ success: false, error: "Can't find Group..!!" });
     }
     return isGroupExcist;
-}
+};
 
 export const isUserAdmin = async (groupData, userId) => {
     if (!groupData.adminId.includes(userId)) {
         return res.status(404).json({ success: false, error: "You're not an admin..!!" });
     }
     return true;
-}
+};

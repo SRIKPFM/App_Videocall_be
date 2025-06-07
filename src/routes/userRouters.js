4f6e7b86b7f58f7022781c943a12884f0f06108d
Sundaresh-ishop <sundareshishop@gmail.com>
2025-06-07 17:17:35 +0530

    api for profile picture

--- src/routes/userRouters.js
+++ src/routes/userRouters.js
@@ -1,40 +1,78 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
+import { v2 as cloudinary } from 'cloudinary';
+import multer from 'multer';
+import path from 'path';
+import fs from 'fs';
 import { UserLoginCredentials } from '../models/loginModels.js';
 import { ContactDetails } from '../models/contactsModels.js';
-import { generateToken, getUserDataFromToken, getUserIdFromToken } from '../Helper/helper.js';
+import { generateToken, getFolderByMimeType, getUserDataFromToken, getUserIdFromToken } from '../Helper/helper.js';
 import { authendicate } from '../middleware/middleware.js';
 
 const router = express.Router();
+
+cloudinary.config({
+    cloud_name: "divinwelb",
+    api_key: "731699916219541",
+    api_secret: "4VIx6OnQAaDr3tQjdHpaFgRWOdA"
+});
+
+const upload = multer({ dest: 'temp/' });
 
 router.get('/api/checkServer', async (req, res) => {
     console.log("Server is running...");
 });
 
-router.post('/api/userRegister', async (req, res) => {
+router.post('/api/userRegister', upload.single('profile'), async (req, res) => {
     try {
         const { userId, userName, email, password, confirmPassword } = req.body;
+
         const isUserExcist = await UserLoginCredentials.findOne({ userId: userId, email: email });
         if (isUserExcist) {
             return res.status(400).json({ success: false, error: "This email already registered.." });
-        } else {
-            if (password === confirmPassword) {
-                const userStructure = new UserLoginCredentials({
-                    userId: userId,
-                    userName: userName,
-                    email: email,
-                    password: password,
-                    fcmToken: null,
-                    isLoggedin: false,
-                    passwordForArchive: null
-                })
-                const createUser = await UserLoginCredentials.create(userStructure)
-                    .then(() => { return res.status(200).json({ success: true, message: "User registered successfully..!!" }) });
-            } else {
-                return res.status(404).json({ success: false, error: "Enter the correct passowrd..!!" });
-            }
         }
+
+        if (password !== confirmPassword) {
+            return res.status(400).json({ success: false, error: "Passwords do not match." });
+        }
+
+        let profilePicUrl = null;
+
+        if (req.file) {
+            const filePath = req.file.path;
+            const mimetype = req.file.mimetype;
+
+            const folder = getFolderByMimeType(mimetype);
+
+            const originalNameWithExt = path.parse(req.file.originalname);
+
+            const result = await cloudinary.uploader.upload(filePath, {
+                resource_type: 'image',
+                folder: `ProfilePicture/Individual/${userName}-${userId}`,
+                public_id: `${userName}.${userId}_profilePic`
+            });
+
+            fs.unlinkSync(filePath);
+
+            profilePicUrl = result.secure_url;
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const userStructure = new UserLoginCredentials({
+            userId: userId,
+            userName: userName,
+            email: email,
+            password: hashedPassword,
+            fcmToken: null,
+            isLoggedin: false,
+            passwordForArchive: null,
+            profilePicUrl: profilePicUrl
+        })
+        await userStructure.save()
+            .then(() => { return res.status(200).json({ success: true, message: "User register successfully.." }) })
+            .catch((error) => { return res.status(400).json({ success: false, error: error.message }) });
     } catch (error) {
+        console.log(error.message)
         return res.status(500).json({ success: false, error: error.message });
     }
 });
@@ -46,7 +84,10 @@ router.post('/api/userLoginOrLogout', async (req, res) => {
         if (!isUser) {
             return res.status(404).json({ success: false, error: "Couldn't find any user using this email. Enter a valid mail." });
         } else if (type === 'login') {
-            if (isUser.password === password) {
+            console.log(isUser.password)
+            const unHashPassword = await bcrypt.compare(password, isUser.password);
+            console.log(unHashPassword)
+            if (unHashPassword === true) {
                 isUser.isLoggedin = true;
                 await isUser.save();
                 const data = {
@@ -70,9 +111,61 @@ router.post('/api/userLoginOrLogout', async (req, res) => {
 
 router.post('/api/getUser', async (req, res) => {
     try {
+        const { userId } = req.body;
+        const findUserData = await UserLoginCredentials.findOne({ userId: userId });
+        if (!findUserData) {
+            return res.status(404).json({ success: false, error: "User not found..!!" });
+        }
+        return res.status(200).json({ success: true, data: findUserData });
+    } catch (error) {
+        return res.status(500).json({ success: false, error: error.message });
+    }
+});
+
+router.post('/api/updateUserDetails', authendicate, upload.single('file'), async (req, res) => {
+    try {
         const token = req.header('Authorization');
-        const userData = await getUserDataFromToken(token);
-        return res.status(200).json({ success: true, userData });
+        if (!token) {
+            return res.status(400).json({ success: false, error: "Token required..!!" });
+        }
+        const userId = await getUserIdFromToken(token);
+        const updates = req.body;
+
+        const findUserData = await UserLoginCredentials.findOne({ userId: userId });
+        if (!findUserData) {
+            return res.status(404).json({ success: false, error: "Can't find user details." });
+        }
+
+        if (req.file) {
+            if (findUserData.profilePicUrl) {
+                const publicId = findUserData.profilePicUrl.split('/').slice(-1)[0].split('.')[0];
+                console.log(publicId);
+
+                await cloudinary.uploader.destroy(
+                    `ProfilePicture/Individual/${findUserData.userName}-${findUserData.userId}/${publicId}`,
+                    { resource_type: 'image' }
+                );
+            }
+
+            const filePath = req.file.path;
+            const result = await cloudinary.uploader.upload(filePath, {
+                resource_type: 'image',
+                folder: `ProfilePicture/Individual/${findUserData.userName}-${findUserData.userId}`,
+                public_id: `${findUserData.userName}-${findUserData.userId}_profilePic`
+            });
+
+            fs.unlinkSync(filePath);
+
+            updates.profilePicUrl = result.secure_url;
+        }
+        delete updates.file;
+        delete updates.userId;
+
+        const updateUserDetails = await UserLoginCredentials.findOneAndUpdate({ userId: userId }, { $set: updates }, { new: true });
+        if (!updateUserDetails) {
+            return res.status(400).json({ success: false, error: "Can't update user details." });
+        }
+        return res.status(200).json({ success: true, message: "User details successfully updated..!!" });
     } catch (error) {
         return res.status(500).json({ success: false, error: error.message });
     }
@@ -309,7 +402,7 @@ router.post('/api/getFavoriteUsers', authendicate, async (req, res) => {
         if (!isUserExcist) {
             return res.status(404).json({ success: false, error: "User not found..!!" });
         }
-        return res.status(200).json({ success: true , favoriteUserId: isUserExcist.favoritedUser });
+        return res.status(200).json({ success: true, favoriteUserId: isUserExcist.favoritedUser });
     } catch (error) {
         return res.status(500).json({ success: false, error: error.message });
     }
